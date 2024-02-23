a0688d2547caf3d6d53716d13ab960945462631b
Marsh Macy <mmacy@users.noreply.github.com>
2024-02-22 21:27:24 -0800

    Deployed a73aa41 with MkDocs version: 1.5.3

--- reference/include/openai_init.py
+++ reference/include/openai_init.py
@@ -8,15 +8,34 @@ including large language models (LLMs) like GPT-4 and models for working with im
 provides both synchronous and asynchronous API clients, options to configure their behavior, and modules that provide
 Python code with an API surface to interact with the OpenAI platform.
 
-To get started, check out the documentation for the module representing the [resource][src.openai.resources] you're interested in using for your
-project. For example, the [`resources.chat.completions`][src.openai.resources.chat.completions] module is what you'd use
+To get started, read the submodule descriptions in [`resources`][src.openai.resources] to determine which best fits your
+project. For example, the [`resources.chat`][src.openai.resources.chat] submodule description indicates it's a good fit
 for conversational chat-style interactions with an LLM like [GPT-4 Turbo](https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo).
-Or, maybe you need the [`resources.audio`][src.openai.resources.audio] module for performing audio transcription, translation, and
-speech synthesis in your app.
+Or, maybe you need the [`resources.audio`][src.openai.resources.audio] module to perform audio transcription,
+translation, and speech synthesis in your app.
 
-Documentation for the library's main API client classes, [`OpenAI`][src.openai.OpenAI] and
-[`AsyncOpenAI`][src.openai.AsyncOpenAI], is another good place to start. The clients are the primary contact point for
-your code that needs to work with any of the resources available on OpenAI API endpoints.
+Once you've determined the resource to use, create an [`OpenAI`][src.openai.OpenAI] or [`AsyncOpenAI`][src.openai.AsyncOpenAI]
+client instance and access the instance attribute for that resource on the client object. For example, if you instantiate
+an `OpenAI` client object named `client`, you'd access the [`OpenAI.chat`][src.openai.OpenAI.chat] instance attribute:
+
+```python
+from openai import OpenAI
+
+# Reads API key from OPENAI_API_KEY environment variable
+client = OpenAI()
+
+# Use the `chat` resource to interact with the OpenAI chat completions endpoint
+completion = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        },
+    ],
+)
+print(completion.choices[0].message.content)
+```
 
 For more information about the REST API this package talks to or to find client libraries for other programming
 languages, see:
